aac266079a77185fb0dfec07619362aefad034b3
Walsh D'Souza <walshdsouza10@gmail.com>
2026-09-17 23:11:43 +0530

    Peak Index Mountain Array

---
+++ Leetcode/Q852.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int st=1, end=arr.size()-2;
+
+        while(st<=end){
+            int mid=st+(end-st)/2;
+
+            if(arr[mid-1]<arr[mid] && arr[mid]>arr[mid+1]){
+                return mid;
+            }
+            else if(arr[mid-1]<arr[mid])//right
+            {
+                st=mid+1;
+            }
+            else{//left
+                end=mid-1;
+            }
+
+        }
+
+        return -1;
+    }
+};
